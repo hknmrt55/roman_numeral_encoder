f0d35bf3aff30d14aa6643c432293ab6f484e9d1
hknmrt61 <117194919+hknmrt55@users.noreply.github.com>
2023-12-27 23:13:27 +0300

    Add files via upload

---
+++ RomanEncoder.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+std::string Encoder(int number)
+{
+    std::string encoded;
+    int numbers[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    std::string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+  
+    for (int i = 0; i <= 12; i++) 
+    {
+      while (number >= numbers[i]) 
+      {
+        number -= numbers[i];
+        encoded += symbols[i];
+      }
+    }
+
+    return encoded;
+}
+
+int main()
+{
+    int number;
+
+    while (true)
+    {
+        std::cout << "Enter a number between 0 and 3999: ";
+        std::cin >> number;
+
+        if (number >= 0 && number <= 3999)
+        {
+            break; // Exit the loop if the input is valid
+        }
+        std::cout << "Invalid input. Please enter a number between 0 and 3999." << std::endl;
+         std::cin.clear();  // Clear the error flag
+
+    }
+
+    std::cout << "\n";
+    std::cout << "Given number "<< number << " with Roman numerals is: " << Encoder(number) << std::endl;
+
+    return 0;
+}
